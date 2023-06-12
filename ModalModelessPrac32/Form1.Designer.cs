75bca398e66ebfe9419dd0540668ed9a9b9e01a7
Jimin Baek <qorwlalsjimin@naver.com>
2023-06-12 12:08:19 +0900

    메시지박스를 활용한 모달창 띄우기 및 버튼입력 받기

--- ModalModelessPrac32/Form1.Designer.cs
+++ ModalModelessPrac32/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 94);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ModalModelessPrac32/Form1.cs
+++ ModalModelessPrac32/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,43 @@ namespace ModalModelessPrac32
 {
     public partial class Form1 : Form
     {
+        // 속성을 덮어쓴 것
+        class CustomForm : Form
+        {
+            public CustomForm()
+            {
+                this.Text = "제목 글자";
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 순서대로 뜬다
+            // 모달창 말고 다른 것은 클릭 불가
+            MessageBox.Show("내용");
+            MessageBox.Show("내용", "제목");
+
+            DialogResult result;
+            // 사용자한테 입력 받기
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel);
+            } while (result == DialogResult.Cancel); //반복 시키라는 뜻
+
+            if(result == DialogResult.Cancel)
+            {
+                MessageBox.Show("사용자가 취소하였습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
         }
     }
 }
